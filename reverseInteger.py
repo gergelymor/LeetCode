2f67cc0755db47214d81cdb4c490626c30d3ec75
gergelymor <mor.mango@gmail.com>
2022-08-30 21:04:22 +0200

    Create reverseInteger.py

---
+++ reverseInteger.py
@@ -0,0 +1,12 @@
+#7. Reverse Integer
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        stringValue = str(x)
+        intValueReversed = stringValue[::-1]
+        if stringValue[0] == "-":
+            intValueReversed = "-" + intValueReversed[:-1]
+        ret = int(intValueReversed)
+        if ret < -2**31 or ret > (2**31)-1:
+            return 0
+        return ret
