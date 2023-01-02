65fdc74f43d7f3f8b87d496ea9238492dc97b87c
gergelymor <mor.mango@gmail.com>
2023-01-02 21:21:17 +0100

    Update detectCapital.py

--- detectCapital.py
+++ detectCapital.py
@@ -4,9 +4,9 @@ class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         for i, char in enumerate(word):
             if char.isupper():
-                if i>0 and len(word) > i+1 and word[i+1].islower():
+                if i > 0 and len(word) > i+1 and word[i+1].islower():
                     return False
             elif char.islower():
-                if len(word)>i+1 and word[i+1].isupper():
+                if len(word) > i+1 and word[i+1].isupper():
                     return False
         return True
